ef1758a118ac6638e20d5153a5ede4cb14ae9fc5
xaviola123 <151661629+xaviola123@users.noreply.github.com>
2024-04-12 10:26:38 +0200

    Add files via upload

--- IdeaProjects/orquesta/src/Orquesta.java
+++ IdeaProjects/orquesta/src/Orquesta.java
@@ -1,16 +1,29 @@
 import com.orquesta.classes.Flauta;
 import com.orquesta.classes.GuitarraElectrica;
+import com.orquesta.classes.Instrumento;
 import com.orquesta.classes.Tambor;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Orquesta {
     public static void main(String[] args) {
-    Flauta flauta = new Flauta("Saxofon \uD83C\uDFB7","Viento","metalico");
-    flauta.tocar();
-    GuitarraElectrica guitarra = new GuitarraElectrica("Guitarra \uD83C\uDFB8","Electrica",250);
-    guitarra.tocar();
-    Tambor tambor = new Tambor("Tambor","Percusion",5);
-    tambor.aporrear();
-   
 
+        List<Instrumento> instrumentos = new ArrayList<>();
+
+
+        instrumentos.add(new Flauta("Saxofon \uD83C\uDFB7", "Viento", "metalico"));
+        instrumentos.add(new GuitarraElectrica("Guitarra \uD83C\uDFB8", "Electrica", 250));
+        instrumentos.add(new Tambor("Tambor", "Percusion", 5));
+        
+        for (Instrumento instrumento : instrumentos) {
+            if (instrumento instanceof Flauta) {
+                ((Flauta) instrumento).tocar();
+            } else if (instrumento instanceof GuitarraElectrica) {
+                ((GuitarraElectrica) instrumento).tocar();
+            } else if (instrumento instanceof Tambor) {
+                ((Tambor) instrumento).aporrear();
+            }
+        }
     }
 }
